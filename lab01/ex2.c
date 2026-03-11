5f341576a1f9d72afacc5046c71cab76fe222ccb
Everton Pereira Militão <97249995+xXeverton@users.noreply.github.com>
2026-03-11 21:07:40 +0000

    add ex2 - lab01

--- lab01/ex2.c
+++ lab01/ex2.c
@@ -8,7 +8,9 @@
 int main(void)
 {
     int mypid, pid, status;
+    int numero = 1;
 
+    printf("numero = %d (antes do processo pai)\n", numero);
     pid = fork();
 
     if (pid != 0)
@@ -16,15 +18,19 @@ int main(void)
         mypid = getpid();
         printf("Parent PID: %d\n", mypid);
         waitpid(-1, &status, 0);
-
+        printf("numero = %d (apos processo filho)\n", numero);
+        
         if (!WIFEXITED(status)) printf("O processo nao terminou corretamente\n");
 
     }
     else
     {
+
+        numero = 5;
         mypid = getpid();
         printf("Child PID: %d\n", mypid);
         
+        printf("numero = %d (durante processo filho)\n", numero);
         exit(0);
     }
 
