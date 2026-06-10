cc658ef93f05287c605862d6c85b4b3c1ec5782f
Everton Pereira Militao <everton.militao@outlook.com.br>
2026-06-09 21:58:45 -0300

    add & update: adicionando lab10

--- lab10/log_gen.c
+++ lab10/log_gen.c
@@ -1,30 +1,35 @@
+#define MAX_KEY 999
+#define MIN_KEY 0
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_KEY 999
-#define MIN_KEY 0
-
-typedef enum {
-    READ, WRITE
+typedef enum
+{
+    READ,
+    WRITE
 } OpType;
 
-// Função baseada nos slides para gerar as operações
-void gerar_operacoes(int n) {
+
+// Função que gera e exibe N operações aleatórias
+void gerar_operacoes(int n)
+{
     // Inicializa o gerador de números aleatórios com o tempo atual
     srand(time(NULL));
-    
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         // Escolhe aleatoriamente entre READ (0) e WRITE (1)
         OpType op = rand() % 2;
-        
         // Gera key aleatória no intervalo de 0 a 999
         int key = (rand() % (MAX_KEY - MIN_KEY + 1)) + MIN_KEY;
-        
-        if (op == READ) {
+        if (op == READ)
+        {
             // Formato pedido: R, key
             printf("R, %d\n", key);
-        } else {
+        }
+        else
+        {
             // Gera um valor inteiro aleatório qualquer (ex: entre 10 e 5000)
             int value = (rand() % 4991) + 10;
             // Formato pedido: W, value, key
@@ -32,6 +37,7 @@ void gerar_operacoes(int n) {
         }
     }
 }
+
 
 int main() {
     // Redireciona a saída padrão (printf) para o arquivo "log.txt" no modo escrita ("w")
